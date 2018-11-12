ad5d82518b5edfdb0d197762eda4d12f2b20c93d
ketty <72768163ketty@gmail.com>
2018-11-12 17:26:55 -0400

    infra add docker and vagrant

---
+++ vagrant/environment.bat
@@ -0,0 +1,12 @@
+@echo off
+set OS_AUTH_URL=http://10.28.3.240:5000/v3
+set OS_IDENTITY_API_VERSION=3
+set OS_TENANT_NAME=23652ed22cb846549b599abd7d47b5e0
+set OS_PROJECT_NAME=fj_kettycamacho_at07-desktop
+set OS_USERNAME=kettycamacho
+set OS_PASSWORD=KKK0102018c
+set OS_DOMAIN_NAME=fundacion-jala
+set OS_FLAVOR=f2.desktop.tiny
+set OS_IMAGE=ubuntu-14.04-amd64-server_18062018
+set OS_KEY_PAIR_NAME=windowsk
+set OS_REGION_NAME=bootstack-jalasoft
