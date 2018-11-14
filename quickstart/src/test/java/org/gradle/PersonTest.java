9dd0d820a26aeeaca1b201b47854138f604979e8
ketty01 <72768163ketty@gmail.com>
2018-11-14 18:07:08 -0400

    task#8 unit test

--- quickstart/src/test/java/org/gradle/PersonTest.java
+++ quickstart/src/test/java/org/gradle/PersonTest.java
@@ -1,12 +1,87 @@
 package org.gradle;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PersonTest {
+
+    private Person person;
+
+    @Before
+    public void setUp() throws Exception {
+        person = new Person("Larry", "Joel", "5162738", 7777777, 21, "Simon Lopez", "Single", "Vasquez", "Oruro");
+
+    }
+
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
         assertEquals("Larry", person.getName());
+    }
+
+    @Test
+    public void testLastName() {
+        assertEquals("Joel", person.getLastName());
+    }
+
+    @Test
+    public void test_ci() {
+        assertEquals("5162738", person.getCi());
+    }
+
+    @Test
+    public void test_age() {
+        assertEquals(21, person.getAge());
+    }
+
+    @Test
+    public void test_phone() {
+        assertEquals(7777777, person.getPhone());
+    }
+
+    @Test
+    public void test_address() {
+        assertEquals("Simon Lopez", person.getAddress());
+    }
+
+    @Test
+    public void test_state() {
+        assertEquals("Single", person.getState());
+    }
+
+    @Test
+    public void test_surname() {
+        assertEquals("Vasquez", person.getSurname());
+    }
+
+    @Test
+    public void test_placeBorn() {
+        assertEquals("Oruro", person.getPlaceBorn());
+    }
+
+    @Test
+    public void test_incorrect_name() {
+        assertNotEquals("ketty", person.getName());
+    }
+
+    @Test
+    public void test_incorrect_age() {
+        assertNotEquals(23, person.getAge());
+    }
+
+    @Test
+    public void test_incorrect_lastName() {
+        assertNotEquals("jose", person.getLastName());
+    }
+
+    @Test
+    public void test_incorrect_address() {
+        assertNotEquals("san Martin", person.getAddress());
+    }
+
+    @Test
+    public void test_incorrect_state() {
+        assertNotEquals("married", person.getState());
     }
 }
